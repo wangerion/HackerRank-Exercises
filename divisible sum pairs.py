6c47d87aeefd55be56744cb2c74ddba2ac294b1c
Wangerion <wavesvibe@gmail.com>
2022-11-07 21:58:44 +0200

    Test

--- divisible sum pairs.py
+++ divisible sum pairs.py
@@ -1,17 +1,11 @@
 #https://www.hackerrank.com/challenges/divisible-sum-pairs/problem?isFullScreen=true
-"""
-Trebuie ca suma a 2 cifre din aceasta lista sa fie divizibila cu valoarea lui k.
-"""
 ar = [1, 2, 3, 4, 5, 6]
 k = 5
 n = 6
-count = 0 # tinem cont de cate ori suma este divizibila
-for i in range(len(ar)): # iteram normal prin lista
-	print(f'valaorea lui i este: {i}')
+count = 0 #We keep track of the divisible sum
+for i in range(len(ar)):
 	for j in range(i+1, len(ar)):
-		print(f'valaorea lui j este: {j}')
-		#iteram valoarea lui i + 1 adica in momentul in care ar[i] este la valoarea ar[0] ar[j] va fi la valoarea ar[1]
-		#Astfel niciodata nu se repeta valorile din spate si nu calculam de multe ori aceleasi rezultate
+		#Iterating over the value of i + 1, meaning that as an example when ar[i] is at ar[0], ar[j] will be at ar[1]
 		s = ar[i] + ar[j]
 		if s % k == 0:
 			count += 1
