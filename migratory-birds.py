232886d8d0fc7ef06569adfb451eeb10b0f4da1e
Wangerion <wavesvibe@gmail.com>
2022-11-07 22:50:25 +0200

    Migratory Birds Completed

--- migratory-birds.py
+++ migratory-birds.py
@@ -1,8 +1,15 @@
-arr = [1, 4, 4, 4, 5, 3]
-x = arr.count(arr[0])
-r = []
+arr = [1, 2, 3, 4, 5, 4, 3, 2, 1, 3, 4] #raspunsul trebuie sa fie 3
+r = {}
+a = []
 for i in arr:
-	if arr.count(i) >= x: #We count each value of i and if it's greather than the last value of x we assign it to x.
-		x = arr.count(i)
-		r.append(i)
-print(min(r))
+    #We check if i is in the dict, if it's not we add 1 if it is we start counting the occurences.
+		if i not in r:
+			r[i] = 1
+		else:
+			r[i] += 1
+for i,j in r.items():
+    # We select the keys that have the max value (were counted more)
+    if max(r.values()) == j:
+        a.append(i)
+        
+print(min(a))
