b2685f869419010fb0cfd0ffc7dbdb6dbb20a644
Wangerion <wavesvibe@gmail.com>
2023-04-20 22:34:57 +0300

    Not actual commit.

---
+++ Sales by Match.py
@@ -0,0 +1,12 @@
+def sockMerchant(n, ar):
+    count = 0
+    for i in range(len(ar)):
+        print(ar[i])
+        if ar[i] in ar:
+
+
+if __name__ == '__main__':
+    n = 9
+    ar = [10, 20, 20, 10, 10, 30, 50, 10, 20]
+
+    sockMerchant(n, ar)
